82f035aefbd74d678df27038e67ff1ef63cc5c33
rafael vinicius <royalgames456@gmail.com>
2025-05-18 14:56:38 -0300

    Teste Rafael

---
+++ EsqueciASenhaTeste/EsqueciSenhaHandler.cs
@@ -0,0 +1,19 @@
+﻿using Aplicação.Interfaces_Caso_De_Uso_e_Servicos;
+
+namespace EsqueciASenhaTeste
+{
+    internal class EsqueciSenhaHandler
+    {
+        private IRecuperarSenhaUseCase @object;
+
+        public EsqueciSenhaHandler(IRecuperarSenhaUseCase @object)
+        {
+            this.@object = @object;
+        }
+
+        internal async Task EnviarConfirmacaoAsync(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EsqueciASenhaTeste/ResultadoCasoDeUso.cs
@@ -0,0 +1,14 @@
+﻿using Aplicação.RespostaPadrao;
+using Dominio.Entidades;
+
+namespace EsqueciASenhaTeste
+{
+    internal class ResultadoCasoDeUso : RespostaPadrao<Usuario>
+    {
+        public ResultadoCasoDeUso(bool procede, string mensagem, Usuario dados) : base(procede, mensagem, dados)
+        {
+        }
+
+        public bool Procede { get; set; }
+    }
+}
